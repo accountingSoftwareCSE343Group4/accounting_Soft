a265916daf749b3aa02fba90df817f5f597c7130
alperenbulut <alperenbulut@gtu.edu.tr>
2016-11-05 01:03:42 +0200

    Resolve Conflicts: src/accounting/software/Fuel.java

--- src/accounting/software/Fuel.java
+++ src/accounting/software/Fuel.java
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-=======
 
->>>>>>> refs/remotes/origin/FuelClass
 package accounting.software;
 
 /**
@@ -14,14 +6,7 @@ package accounting.software;
  * @author Ahmet Alperen Bulut
  */
 public class Fuel {
-<<<<<<< HEAD
 
-
-
-=======
-/* fuelType - gasoline - diesel - autogas
-    */
->>>>>>> refs/remotes/origin/FuelClass
  private String fuelType;
  private double fuelCapacity;
  private double  fuelAmount;
@@ -29,9 +14,8 @@ public class Fuel {
  private double salePrice;
  private double tax;
  
-<<<<<<< HEAD
- public  Fuel(String fuelType,double fuelCapacity,double fuelAmount){
-=======
+
+
  public Fuel(){
     this.fuelType="";
     this.fuelCapacity=0;
@@ -42,17 +26,14 @@ public class Fuel {
  }
  public  Fuel(String fuelType,double fuelCapacity,double fuelAmount){
      
->>>>>>> refs/remotes/origin/FuelClass
+
     this.fuelCapacity=fuelCapacity;
     this.fuelAmount=fuelAmount;
     this.fuelType=fuelType;
  
  }
-<<<<<<< HEAD
- public Fuel(){
- 
- }
-     public String getFuelType() {
+
+    public String getFuelType() {
         return fuelType;
     }
 
@@ -74,16 +55,10 @@ public class Fuel {
 
     public double getTax() {
         return tax;
-=======
-
-    public String getFuelType() {
-        return fuelType;
->>>>>>> refs/remotes/origin/FuelClass
     }
 
     public void setFuelType(String fuelType) {
         this.fuelType = fuelType;
-<<<<<<< HEAD
     }
 
     public void setFuelCapacity(double fuelCapacity) {
@@ -98,61 +73,21 @@ public class Fuel {
         this.buyingPrice = buyingPrice;
     }
 
-=======
-    } 
-        
-    public double getFuelCapacity() {
-        return fuelCapacity;
-    }
-    
-    public void setFuelCapacity(double fuelCapacity) {
-        this.fuelCapacity = fuelCapacity;
-    }
-    
-    public double getFuelAmount() {
-        return fuelAmount;
-    }
-    
-    public void setFuelAmount(double fuelAmount) {
-        this.fuelAmount = fuelAmount;
-    }
-    
-    public double getBuyingPrice() {
-        return buyingPrice;
-    }
-    
-    public void setBuyingPrice(double buyingPrice) {
-        this.buyingPrice = buyingPrice;
-    }
-    
-    public double getSalePrice() {
-        return salePrice;
-    }
-    
->>>>>>> refs/remotes/origin/FuelClass
     public void setSalePrice(double salePrice) {
         this.salePrice = salePrice;
     }
 
-<<<<<<< HEAD
-=======
-    public double getTax() {
-        return tax;
-    }
-    
->>>>>>> refs/remotes/origin/FuelClass
     public void setTax(double tax) {
         this.tax = tax;
     }
 
     @Override
-<<<<<<< HEAD
     public String toString() {
-=======
-    public String toString() {//added
->>>>>>> refs/remotes/origin/FuelClass
-        return "Fuel{ " + "fuelType= " + fuelType + ", fuelCapacity= " + fuelCapacity + ", fuelAmount= " + fuelAmount + ", buyingPrice= " + buyingPrice + ", salePrice= " + salePrice + ", tax= " + tax + " }";
+        return "Fuel{" + "fuelType=" + fuelType + ", fuelCapacity=" + fuelCapacity + ", fuelAmount=" + fuelAmount + ", buyingPrice=" + buyingPrice + ", salePrice=" + salePrice + ", tax=" + tax + '}';
     }
+
+ 
+
    
 }
 
