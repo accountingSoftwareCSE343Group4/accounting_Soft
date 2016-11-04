c99da9d1da64f3e34b37126edc18a162840ebc8c
Ahmet Alperen Bulut <alperenbulut@gtu.edu.tr>
2016-11-04 22:45:37 +0200

    Fuel class (#4)
    * Fuel Class
    Getter-Setter methods
    
    * added toString method

---
+++ src/accounting/software/Fuel.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accounting.software;
+
+/**
+ *
+ * @author Ahmet Alperen Bulut
+ */
+public class Fuel {
+
+
+
+ private String fuelType;
+ private double fuelCapacity;
+ private double  fuelAmount;
+ private double buyingPrice;
+ private double salePrice;
+ private double tax;
+ 
+ public  Fuel(String fuelType,double fuelCapacity,double fuelAmount){
+    this.fuelCapacity=fuelCapacity;
+    this.fuelAmount=fuelAmount;
+    this.fuelType=fuelType;
+ 
+ }
+ public Fuel(){
+ 
+ }
+     public String getFuelType() {
+        return fuelType;
+    }
+
+    public double getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public double getFuelAmount() {
+        return fuelAmount;
+    }
+
+    public double getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public void setFuelCapacity(double fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public void setFuelAmount(double fuelAmount) {
+        this.fuelAmount = fuelAmount;
+    }
+
+    public void setBuyingPrice(double buyingPrice) {
+        this.buyingPrice = buyingPrice;
+    }
+
+    public void setSalePrice(double salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    @Override
+    public String toString() {
+        return "Fuel{ " + "fuelType= " + fuelType + ", fuelCapacity= " + fuelCapacity + ", fuelAmount= " + fuelAmount + ", buyingPrice= " + buyingPrice + ", salePrice= " + salePrice + ", tax= " + tax + " }";
+    }
+   
+}
+
+
