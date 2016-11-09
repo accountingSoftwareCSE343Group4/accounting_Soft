d7b917250d62d8247f80e8a2530cdd4d77cadab2
Mehmet Gürol Çay <gurolcay123@gmail.com>
2016-11-10 00:48:06 +0300

    initialize accounting system class

---
+++ src/accounting/software/AccountingSystem.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accounting.software;
+
+import java.util.List;
+
+/**
+ *
+ * @author gurol
+ */
+public abstract class AccountingSystem {
+    private List<Personnel> personnelList;
+    private List<Fuel> fuelList;
+}
