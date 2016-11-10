d611800bc48bbd750d2349b2bfe8e6ba4709f496
alperenbulut <alperenbulut@gtu.edu.tr>
2016-11-10 21:02:33 +0300

    update

--- src/accounting/software/AccountingSystem.java
+++ src/accounting/software/AccountingSystem.java
@@ -1,16 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package accounting.software;
 
+package accounting.software;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
- * @author Arif Dogru
+ * @author Ahmet Alperen Bulut
  */
 public class AccountingSystem {
     List<Personnel> personnelList;
