81401e09448c2a40e3a3e07a42d0197efb2f1102
Mehmet Gürol Çay <gurolcay123@gmail.com>
2016-11-10 23:07:50 +0300

    Merge branch 'jsonParser' into develop
    # Conflicts:
    #	src/accounting/software/Fuel.java

---
+++ src/accounting/software/Personal.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accounting.software;
+
+/**
+ *
+ * @author gurol
+ */
+public class Personal {
+    private int id;
+    private String name;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+
+    public Personal(int id, String name, String lastName, String address, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+   
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "id:" + id + "name:" + name + "lastName:" + lastName + "address:" + address + "phoneNumber:" + phoneNumber;
+    }
+}
