8f1014adea57db046c6db37d01a0a02a930d4953
alperenbulut <alperenbulut@gtu.edu.tr>
2016-11-10 20:19:53 +0300

    Fuel edit

--- src/accounting/software/Fuel.java
+++ src/accounting/software/Fuel.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 package accounting.software;
 
 /**
@@ -15,8 +13,6 @@ public class Fuel implements Expenses{
  private double salePrice;
  private double tax;
  
-
-
  public Fuel(){
     this.fuelType="";
     this.fuelCapacity=0;
@@ -25,9 +21,9 @@ public class Fuel implements Expenses{
     this.salePrice=0;
     this.tax=0;
  }
+ 
  public  Fuel(String fuelType,double fuelCapacity,double fuelAmount){
      
-
     this.fuelCapacity=fuelCapacity;
     this.fuelAmount=fuelAmount;
     this.fuelType=fuelType;
@@ -91,101 +87,3 @@ public class Fuel implements Expenses{
 
    
 }
-
-
-=======
-
-package accounting.software;
-
-/**
- *
- * @author Ahmet Alperen Bulut
- */
-public class Fuel implements Expenses{
-
- private String fuelType;
- private double fuelCapacity;
- private double  fuelAmount;
- private double buyingPrice;
- private double salePrice;
- private double tax;
- 
-
-
- public Fuel(){
-    this.fuelType="";
-    this.fuelCapacity=0;
-    this.fuelAmount=0;
-    this.buyingPrice=0;
-    this.salePrice=0;
-    this.tax=0;
- }
- public  Fuel(String fuelType,double fuelCapacity,double fuelAmount){
-     
-
-    this.fuelCapacity=fuelCapacity;
-    this.fuelAmount=fuelAmount;
-    this.fuelType=fuelType;
- 
- }
-
-    public String getFuelType() {
-        return fuelType;
-    }
-
-    public double getFuelCapacity() {
-        return fuelCapacity;
-    }
-
-    public double getFuelAmount() {
-        return fuelAmount;
-    }
-
-    public double getBuyingPrice() {
-        return buyingPrice;
-    }
-
-    public double getSalePrice() {
-        return salePrice;
-    }
-
-    public double getTax() {
-        return tax;
-    }
-
-    public void setFuelType(String fuelType) {
-        this.fuelType = fuelType;
-    }
-
-    public void setFuelCapacity(double fuelCapacity) {
-        this.fuelCapacity = fuelCapacity;
-    }
-
-    public void setFuelAmount(double fuelAmount) {
-        this.fuelAmount = fuelAmount;
-    }
-
-    public void setBuyingPrice(double buyingPrice) {
-        this.buyingPrice = buyingPrice;
-    }
-
-    public void setSalePrice(double salePrice) {
-        this.salePrice = salePrice;
-    }
-
-    public void setTax(double tax) {
-        this.tax = tax;
-    }
-
-    @Override
-    public String toString() {
-        return "Fuel{" + "fuelType=" + fuelType + ", fuelCapacity=" + fuelCapacity + ", fuelAmount=" + fuelAmount + ", buyingPrice=" + buyingPrice + ", salePrice=" + salePrice + ", tax=" + tax + '}';
-    }
-
- 
-
-   
-}
-
-
->>>>>>> origin/master
