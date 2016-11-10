0e5ef235464216e597beceee35070bd2a145f69f
Ahmet Alperen Bulut <alperenbulut@gtu.edu.tr>
2016-11-10 23:17:12 +0200

    added getInstance (#18)
    * Instance-getInstance

--- src/accounting/software/AccountingSystem.java
+++ src/accounting/software/AccountingSystem.java
@@ -13,17 +13,13 @@ import java.util.List;
  * @author Ahmet Alperen Bulut
  */
 public class AccountingSystem {
-    List<Personnel> personnelList;
-    List<Fuel> fuelList;
-    
-    public AccountingSystem(){
-        personnelList=new ArrayList();
-        fuelList=new ArrayList();
+    List<Personnel> personnelList=new ArrayList();
+    List<Fuel> fuelList=new ArrayList();
+    private static final AccountingSystem INSTANCE = new AccountingSystem();
+    private AccountingSystem(){}
+    public static AccountingSystem getInstance(){
+        return INSTANCE;
     }
-    /* Members */
-    
-    
-    /* Methods */
     public void addPerson(Personnel newPerson)
     {
         boolean duplicate=false;
