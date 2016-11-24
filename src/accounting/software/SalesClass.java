77c8cf5bf46078ae52a42532cf49ac286ed4f390
Z Ozge YILMAZ <zubeydeozgeyilmaz@gmail.com>
2016-11-24 17:42:29 +0300

    salesClass is added date and override toString method.

--- src/accounting/software/SalesClass.java
+++ src/accounting/software/SalesClass.java
@@ -5,6 +5,9 @@ package accounting.software;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
@@ -13,14 +16,38 @@ package accounting.software;
 public class SalesClass implements Income{
     String description;
     int ID;
-    int price;
+    Double price;
+    private Date saleDate;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy"); // format is 21/11/2016
 
-    public SalesClass(String description, int ID, int price) {
+
+    public SalesClass(String description, int ID, Double price,String date) {
         this.description = description;
         this.ID = ID;
         this.price = price;
+        saleDate = new Date();
+        try{
+            this.saleDate = dateFormat.parse(date);
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }
+
+    }
+    public Date getSaleDate(){
+        return saleDate;
     }
     
+    public void setSaleDate(String date){
+        try{
+            this.saleDate = dateFormat.parse(date);
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }        
+    }
+
+    @Override
     public String getDescription() {
         return description;
     }
@@ -37,21 +64,26 @@ public class SalesClass implements Income{
         this.ID = ID;
     }
 
-    public int getPrice() {
+    public Double getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
+    public void setPrice(Double price) {
         this.price = price;
     } 
 
     @Override
     public Double getIncome() {
-        return new Double(getPrice());
+        return getPrice();
     }
 
     @Override
     public String getName() {
         return "Sale";
+    }
+    
+    @Override
+    public String toString(){
+        return "Date = " + dateFormat.format(saleDate) + " Description = " + description + " price = " + price ;
     }
 }
