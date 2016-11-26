c67608aea843cc5ffa0e1ad5a63a4da57d29c235
Emre Bayram <emre.gss@gmail.com>
2016-11-26 16:43:14 +0300

    ..

--- src/accounting/software/Personnel.java
+++ src/accounting/software/Personnel.java
@@ -112,4 +112,9 @@ public class Personnel implements Expenses{
         return "Employee_Expense";
     }
 
+    @Override
+    public Date getDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
